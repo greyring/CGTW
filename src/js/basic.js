25b6dd2908d41c11d2d711da1b9583ea95f20110
GreyRing <greyring@163.com>
2017-12-24 17:21:16 +0800

    js完成

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,6 +1,8 @@
-onload = function() {
-    var c = document.getElementById("glcanvas");
+$(function() {
+    $('#leftMenu').metisMenu();      
+    $('#rightMenu').metisMenu();
+    var c = $("#glcanvas")[0];
     var gl = c.getContext('webgl');
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-};
+});
